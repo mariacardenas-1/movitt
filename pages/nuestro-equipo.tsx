a62153e389c82187667e31941b1554ae60c0c41d
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-05-26 14:54:53 -0300

    fix team card spacing

--- pages/nuestro-equipo.tsx
+++ pages/nuestro-equipo.tsx
@@ -8,7 +8,9 @@ export default function NuestroEquipo () {
             <TextSection 
               tittle={'Nuestro Equipo'} 
               text={'La capacidad de nuestra compañía permite ofrecer servicios integrales en estudios, diseños e interventoría de proyectos de transporte, tránsito e infraestructura, conformado por personal especializado, con experiencia y disposición de ofrecer las mejores alternativas en el campo de evaluación y análisis para el desarrollo de proyectos urbanos y regionales de carácter público o privado.'} />
-            <TableValue />
+            <div className="pb-10">
+                <TableValue />
+            </div>
         </Layout>
     )
 }
