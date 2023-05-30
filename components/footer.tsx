4922cc29d84f658a73c069e839db1c4f12b553bd
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-05-30 20:52:39 -0300

    fixing whatsapp button

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const Footer = () => {
   const date = new Date().getFullYear()
   return (
-    <footer className="bg-zinc-900 h-24 text-white px-5 md:px-40 items-center lg:py-8 py-14 justify-center md:justify-between flex flex-col md:flex-row ">
+    <footer className="bg-zinc-900 h-24 text-white px-5 md:px-40 items-start md:items-center lg:py-8 py-14 justify-center md:justify-between flex flex-col md:flex-row ">
       <div className='flex gap-3'>
         <Link target='_blank' href='https://www.linkedin.com/in/movitt-sas-81137a171/' >
           <Image src={linkedIn} alt='linkedIn' className='w-7 pb-3 md:pb-0 ' />
@@ -15,10 +15,11 @@ const Footer = () => {
           <Image src={instagramIcon} alt='instagram' className='w-7 pb-3 md:pb-0 ' />
         </Link>
       </div>
-      <div className='text-neutral-500'>
+      <div className='text-neutral-500 '>
       Movitt {date} -
       Desarrollado por:&nbsp;
         <Link target='_blank' href='https://www.linkedin.com/in/maria-fernanda-c%C3%A1rdenas-1-frontend/' className='text-neutral-300 hover:text-neutral-50'>
+          <br/>
           Mafe Cárdenas
         </Link>
       </div>
--- components/whatsappLink.tsx
+++ components/whatsappLink.tsx
@@ -4,8 +4,8 @@ import whatsapp from '../public/assets/whatsappIcon.png'
 
 export default function WhatsappLink () {
     return (
-        <Link className='p-3 fixed bottom-24 right-0 lg:bottom-24 lg:right-24' target='_blank' href={'https://wa.link/o3s2ib'} >
-            <Image className='w-10 h-10' src={whatsapp} alt="whatsapp"/>
+        <Link className='p-4 fixed bottom-0 right-0 desk:bottom-24 desk:right-24' target='_blank' href={'https://wa.link/o3s2ib'} >
+            <Image className='w-20 h-20 desk:w-16 desk:h-16' src={whatsapp} alt="whatsapp"/>
         </Link>
     )
 }
